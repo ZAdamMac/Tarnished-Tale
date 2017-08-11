eafaa0bd03502d7988b7efa4d411efb2cc956f66
ZAdammac <zachary.j.adam@gmail.com>
2017-08-11 10:24:40 -0300

    Puts server-dev.py into a known-working state. To be used for basic IO testing with the client protocol.
    Signed-off-by: ZAdammac <zachary.j.adam@gmail.com>

--- server-dev.py
+++ server-dev.py
@@ -1,22 +1,23 @@
-#Tarnished Tale Server
-#Modernized, Security-Conscious MUD Client
-#For full details, view the docs
+# Tarnished Tale Server
+# Modernized, Security-Conscious MUD Client
+# For full details, view the docs
 
-#Importing Key Modules
+# Importing Key Modules
 import asyncio
 import websockets as ws
 
-#Defining Principal Classes
+# Defining Principal Classes
 
-#Defining Functions
+# Defining Functions
 async def echo(sock, port):  # A simple echo function currently included for testing reasons
     msg = await sock.recv()
     print("I have received %s" % msg)
     await sock.send(msg)
 
-#Initialize the Config Parser&Fetch Globals
+# Initialize the Config Parser&Fetch Globals
+global portIn; portIn = 5050 #The incoming port, currently defined hardcoded for testing, will be configurable var
 
-#Runtime Time
-start_server = ws.serve(echo, 'localhost', portIn)
+# Runtime Time
+start_server = ws.serve(echo, 'localhost', portIn) #The server will always be listening for incoming commands so it can listen on localhost
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
