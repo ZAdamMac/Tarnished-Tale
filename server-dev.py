2073fc77ce1d02c7f072213fe8305ae7b867c40a
ZAdammac <zachary.j.adam@gmail.com>
2017-08-11 15:54:56 -0300

    Added some configuration/data frameworks in the backend --Tarnished-Core.dat to handle data for the Core Module --server_config.txt as general per-instance configuration Corrected a port addressing issue in clientWeb.html (now working)
    Expansion of server-dev.py:
    server-dev.py no longer a pure echo server. Begun development
    toward the v0.1.0 benchmark.
    -Fetch config vars from config, now
    -Engine reports the loaded game (on every message for now, TODO fix)
    
    Signed-off-by: ZAdammac <zachary.j.adam@gmail.com>

--- server-dev.py
+++ server-dev.py
@@ -4,21 +4,43 @@
 
 # Importing Key Modules
 import asyncio
+import configparser
+import multiprocessing as mp
+import os
 import websockets as ws
 
 # Defining Principal Classes
 
 # Defining Functions
-async def echo(sock, port):  # A simple echo function currently included for testing reasons
-    print("Listening on port: %s" % port)
-    msg = await sock.recv()
-    print("I have received %s" % msg)
-    await sock.send(msg)
+async def serve(sock, port):  # The basic runtime of the entire server goes into this function.
+    while True:  # This is stupid, never do this.
+        print("New Connection on port: %s" % port)
+        await sock.send("Welcome to: %s" % ns.title)  # TODO This needs to become an on-connect event!
+        msg = await sock.recv()
+        print("I have received %s" % msg)
+        await sock.send(msg)
 
 # Initialize the Config Parser&Fetch Globals
-global portIn; portIn = 5050 # The incoming port, currently defined hardcoded for testing, will be configurable var
+abspathHome = os.getcwd()
+abspathBaseConfig = os.path.join(abspathHome, "Configuration/server_config.txt")
+abspathModDats = os.path.join(abspathHome, "Configuration/Module_Files")
+
+baseConfig = configparser.ConfigParser()   # We need several parsers. This one will handle the basic config file.
+baseConfig.read(abspathBaseConfig)
+
+moduleConfig = configparser.ConfigParser()
+# This reader will need to iterate over any and all .dat files in the Configuration/Module Files dir and integrate them
+# into one namespace
+
+if __name__ == '__main__':  # We also need to set up a manager process and a namespace for future builds
+    manager = mp.Manager()
+    ns = manager.Namespace()
+    global ns
+
+    ns.title = baseConfig.get("Game Information", "Game Name")
+    ns.portIn = baseConfig.get("Network Configuration", "Incoming Port")
 
 # Runtime Time
-start_server = ws.serve(echo, 'localhost', portIn) #The server will always be listening for incoming commands so it can listen on localhost
+start_server = ws.serve(serve, 'localhost', ns.portIn)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
